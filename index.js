a536ffd7ea1c77fd13854ef649dbeb8658a8b499
Moses <moses.oyelade@student.moringaschool.com>
2023-07-26 18:58:00 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+  
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         debugger;
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+const names = ["Alfred", "Tobiloba", "Jayden", "Moses"];
+const event = "christmas";
+
+function writeCards(names, event) {
+    let message = [];
+    for (let i = 0; i < names.length; i++){
+        
+        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+
+        debugger;
+    }
+    
+    return message;
+}
+  
+
+function countDown(){
+    let i = 0;
+    while (i < 10){
+        console.log(i);
+        i++;
+    }
+    console.log(i);
+}
+
